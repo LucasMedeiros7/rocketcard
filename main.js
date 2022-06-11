499e9a8528ad22d355837ad415e4b856c3bac91c
Lucas Medeiros <lucaasmedeiros7@gmail.com>
2022-06-11 17:05:02 -0300

    Refatorando =>  main.js

--- main.js
+++ main.js
@@ -1,39 +1,26 @@
-$('form').submit(false);
-const input = $('input')
+$("form").submit(false);
+const input = $("input");
 
-$('button').click(() => {
-  if (input.val() == '') {
-    return alert("Insira um nome de usuário")
+$("button").click(() => {
+  if (input.val() == "") {
+    return alert("Insira um nome de usuário");
   }
-
-  $.ajax({
-    url: `https://api.github.com/users/${input.val()}/following`,
-    success(resposta){
-      const seguindo = resposta.length
-      $('#seguindo').html(seguindo)
-    }
-  })
-
-  $.ajax({
-    url: `https://api.github.com/users/${input.val()}/followers`,
-    success(resposta){
-      const seguidores = resposta.length
-      $('#seguidores').html(seguidores)
-    }
-  })
-
   $.ajax({
     url: `https://api.github.com/users/${input.val()}`,
     success(resposta) {
-      const nome = resposta.name
-      const pfp = resposta.avatar_url
-      const repositorio = resposta.public_repos
-      const localizacao = resposta.location
+      const nome = resposta.name;
+      const seguindo = resposta.following;
+      const seguidores = resposta.followers;
+      const pfp = resposta.avatar_url;
+      const repositorio = resposta.public_repos;
+      const localizacao = resposta.location;
 
-      $('#nome').html(nome)
-      $('#repositorio').html(repositorio)
-      $('#localizacao').html(localizacao)
-      $('#img').attr('src', pfp)
-    }
-  })
-})
+      $("#nome").html(nome);
+      $("#repositorio").html(repositorio);
+      $("#seguidores").html(seguidores);
+      $("#seguindo").html(seguindo);
+      $("#localizacao").html(localizacao);
+      $("#img").attr("src", pfp);
+    },
+  });
+});
